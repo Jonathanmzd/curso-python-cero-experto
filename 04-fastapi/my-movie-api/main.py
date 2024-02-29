d92419f4d087a1d52877d2cb4fcbf193778ed660
mized <mized2009@gmail.com>
2024-02-29 09:34:31 -0500

    04-fastapi - Manejo de errores y middlewares en FastAPI - quitar error

--- 04-fastapi/my-movie-api/main.py
+++ 04-fastapi/my-movie-api/main.py
@@ -79,7 +79,7 @@ def get_movies() -> List[Movie]:
 
 @app.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
-    db = Session
+    db = Session()
     result = db.query(MovieModel).filter(MovieModel.id == id).first()
     if not result:
         return JSONResponse(status_code=404, content={'message': "No encontrado"})
